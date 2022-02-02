035f4f40bdb0c853c77ca9de18c42324686ab3c3
Robin8766 <admin@prodev.pro>
2022-02-02 11:51:54 +0530

    Addec pricing card

--- Components/Cards/Card1.js
+++ Components/Cards/Card1.js
@@ -24,8 +24,8 @@ const Card1 = (props) => {
                     <div className="bg-blue-600 h-2.5 rounded-full" style={{ width: '45%' }}></div>
                 </div>
                 <div className="flex justify-between items-center">
-                    <span className="text-3xl font-bold text-gray-900 dark:text-white">589</span>
-                    <a href="#" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
+                    <span className="whitespace-nowrap text-xl font-bold text-gray-900 dark:text-white">589&nbsp;</span>
+                    <a href="#" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Learn more</a>
                 </div>
             </div>
         </div>
